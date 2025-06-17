a3394ff3dcee61fb47ac1193773254b7476e5131
TomasSiso2000 <112671894+TomasSiso2000@users.noreply.github.com>
2025-06-16 23:50:20 -0300

    Update PlusMinusButton.jsx FLECHA

--- src/components/PlusMinusButton.jsx
+++ src/components/PlusMinusButton.jsx
@@ -1,12 +1,14 @@
 import { TouchableOpacity, Text, StyleSheet } from 'react-native';
 
-export default function PlusMinusButton({ onPress, symbol = '+' }) {
+const PlusMinusButton = ({ onPress, symbol = '+' }) => {
+
   return (
     <TouchableOpacity style={styles.button} onPress={onPress}>
       <Text>{symbol}</Text>
     </TouchableOpacity>
   );
-}
+};
+export default PlusMinusButton;
 
 const styles = StyleSheet.create({
   button: {
